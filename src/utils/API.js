37a3db0417b9b9f048944bd06c26c9377f05a626
Sean Dillon <seandillon1224@gmail.com>
2020-01-24 11:21:45 -0500

    push

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,3 @@
 import axios from "axios";
 
-export const sendEmail = body =>
-  axios.post("http://localhost:3005/api/send", body, {
-    headers: { "Access-Control-Allow-Origin": "*" }
-  });
+export const sendEmail = body => axios.post("/api/send", body);
